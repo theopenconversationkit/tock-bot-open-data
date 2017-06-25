ae667095dbefe09a15727ec31556dca5fe0ad768
Julien Buret <jburet@voyages-sncf.com>
2017-06-25 22:56:22 +0200

    cleanup deploy process
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -5,5 +5,5 @@ echo "tag : $TRAVIS_TAG"
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_TAG" = '' ]; then
 openssl aes-256-cbc -K $encrypted_56cdd536b337_key -iv $encrypted_56cdd536b337_iv -in etc/codesigning.asc.enc -out etc/codesigning.asc -d
 gpg --fast-import etc/codesigning.asc
-mvn clean package dokka:javadocJar deploy -Ddeploy --settings etc/deploy-settings.xml
+mvn dokka:javadocJar deploy -Ddeploy --settings etc/deploy-settings.xml
 fi
