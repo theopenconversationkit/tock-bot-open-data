bcf8b15f94e7a3081fd4bcfbddfe461a49e5bd34
Julien Buret <jburet@voyages-sncf.com>
2020-02-02 23:53:37 +0100

    tock test lib evolution

--- src/test/kotlin/story/GreetingsUnitTest.kt
+++ src/test/kotlin/story/GreetingsUnitTest.kt
@@ -1,29 +1,24 @@
 package ai.tock.bot.open.data.story
 
 import ai.tock.bot.engine.BotBus
-import ai.tock.bot.test.mockMessenger
-import ai.tock.bot.test.test
+import ai.tock.bot.test.mock.mockMessenger
+import ai.tock.bot.test.mock.test
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class GreetingsUnitTest {
-
-
-    @BeforeEach
-    fun beforeEach() {
-        mockMessenger(bus)
-    }
 
     val bus: BotBus = mockk()
 
     @Test
     fun `greetings UT`() {
-        greetings.test(bus)
+        mockMessenger(bus) {
+            greetings.test(bus)
 
-        verify {
-            bus.send("Welcome to the Tock Open Data Bot! :)")
+            verify {
+                bus.send("Welcome to the Tock Open Data Bot! :)")
+            }
         }
 
     }
